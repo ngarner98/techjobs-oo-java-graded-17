bf4bfac02da04230218da1c81ea07d6f095d6d34
ngarner98 <exmachina091098@gmail.com>
2024-01-07 19:10:06 -0600

    Task 4 complete

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,46 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import java.sql.SQLOutput;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class JobTest {
-    //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+
+        assertEquals(testJob.getName(), "Product tester");
+        assertEquals(testJob.getEmployer().getValue(),"ACME");
+        assertEquals(testJob.getLocation().getValue(), "Desert");
+        assertEquals(testJob.getPositionType().getValue(), "Quality control");
+        assertEquals(testJob.getCoreCompetency().getValue(), "Persistence");
+
+        assertTrue(testJob.getName() instanceof String);
+        assertTrue(testJob.getEmployer() instanceof Employer);
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job test1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job test2 =  new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertNotEquals(test1, test2);
+    }
 }
